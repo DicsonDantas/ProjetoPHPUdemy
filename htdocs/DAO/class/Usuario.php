168f543cace095fc8ec22580ea9b3c763015c1dc
DicsonDantas <dicksondantaz@gmail.comgit config --global user.name DicsonDantasgit config --global user Dicksondantaz@gmail.comgit config --global user.email dicksondantaz@gmail.com>
2017-07-23 01:38:21 -0300

    Problema no retorno de Idusuario e Data, vindo com valor NULL

--- htdocs/DAO/class/Usuario.php
+++ htdocs/DAO/class/Usuario.php
@@ -6,7 +6,7 @@ private $deslogin;
 private $dessenha; 
 private $dtcadastro; 
 
- public function getIdisuario(){
+ public function getIdusuario(){
  	return $this->idusuario; 
  }
 
@@ -54,18 +54,76 @@ private $dtcadastro;
  	}
  
 	}
+  
+  public function login($login, $password){
+  	$sql = new Sql();
+  	$sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+        ":LOGIN"=>$login,
+        ":PASSWORD"=>$password
+
+  		));
+  	 if(count($results) > 0) { 
+  	 	
+        $this->setData($results[0]);
+  	 }
+  	 else { 
+  	 	throw new Exception("Login e/ou senha inválidos.");
+  	 	}
+
+  }
+
+  public static function getList(){
+       
+       $sql = new Sql();
+       return $sql->select("SELECT * FROM tb_usuarios order by deslogi Vn");
+  
+
+  }
 
 
-     public function __toString(){
-     	return json_encode(array(
-        "idusuario"=>$this->getIdisuario(),
-        "deslogin"=>$this->getDeslogin(),
-        "dessenha"=>$this->getDessenha(),
-        "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
-     		));
 
-     }
 
-     }
+  public static function search($login){
+  	$sql = new Sql();
+    return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+    	':SEARCH'=>"%".$login."%"));
+
+  }
+
+  //NESTE TRECHO É POSSÍVEL ENTENDER QUE O COMANDO DA UM SELECT ALL NA tb_usuarios Aonde a tupla deslogin for igual a CONSTANTE (que eu ainda não entendi mt bem o pq de as vezes ela ter :: as vezes somente :), cria um array que possui uma outra constante SEARCH, ou utiliza o valor dlea e atribui a login, porém não entendi também o porque desses dois sinais de porcentagem se somente o . é necessario para concatenar. 
+
+
+
+   public function setData($data){
+
+   	$this->setIdusuario($data['idusuario']);
+   	$this->setDeslogin($data['deslogin']);
+   	$this->setDessenha($data['dessenha']);
+   	$this->setDtcadastro(new DateTime($data['dtcadastro'])); 
+
+   }
+
+   public function insert (){
+   	$sql = new Sql();
+   	$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD", array(
+       ':LOGIN'=>$this->getDeslogin(),
+       ':PASSWORD'=>$this->getDessenha()
+   		));
+
+   }
+
+
+
+  public function __toString()
+    {
+        return json_encode(array(
+            "idusuario"=>$this->getIdusuario(),
+            "deslogin"=>$this->getDeslogin(),
+            "dessenha"=>$this->getDessenha(),
+            "dtcadastro"=>$this->getDtcadastro() //->format("d/m/Y H:i:s")
+        ));
+    }
+
+}
 
 	 ?>
--- htdocs/DAO/index.php
+++ htdocs/DAO/index.php
@@ -1,17 +1,33 @@
 <?php 
 
 require_once("config.php");
+
+//CARREGA UM USUARIO
 /*
 $root = new Usuario(); 
-
 $root->loadById(1);
-
 echo ($root); 
-
 */
 
-$sql = new Sql(); 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-echo json_encode($usuarios); 
+
+// CARREGA UMA LIST DE USUARIOS
+// $lista = Usuario::getList();
+// echo json_encode($lista);
+ 
+// CARREGA UMA LISTA DE USUARIOS UTILIZANDO BUSCA NOMINAL PELO LOGIN
+// $search = Usuario::search("dic");
+// echo json_encode($search);
+
+
+
+//DA UM INSERT NO BANCO UTILIZANDO PROCEDURES, CRIA UMA NOVA INSTANCIA DE USUARIO
+$aluno = new Usuario();
+$aluno->setDeslogin("aluno");
+$aluno->setDessenha("@aluno");
+
+$aluno->insert();
+
+echo $aluno;
+
 
  ?>
