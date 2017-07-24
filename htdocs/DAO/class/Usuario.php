582a1e8f196649aef01059a73569a4c2ba93ac11
DicsonDantas <dicksondantaz@gmail.comgit config --global user.name DicsonDantasgit config --global user Dicksondantaz@gmail.comgit config --global user.email dicksondantaz@gmail.com>
2017-07-24 17:35:43 -0300

    Funcionalidades do arquivo Index.PHP todas OK, INSERT, LIST, GET E LOGIN

--- htdocs/DAO/class/Usuario.php
+++ htdocs/DAO/class/Usuario.php
@@ -4,7 +4,9 @@ class Usuario{
 private $idusuario; 
 private $deslogin; 
 private $dessenha; 
-private $dtcadastro; 
+private $dtcadastro;
+
+
 
  public function getIdusuario(){
  	return $this->idusuario; 
@@ -38,49 +40,50 @@ private $dtcadastro;
  	$this->dtcadastro = $value;
  }
 
-
- public function loadById($id) {
- 	$sql = new Sql();
-   $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = : ID", array(":ID"=>$id));
-
- 	if (count($results) >0){
-       
-       $row = $results[0];
-
-       $this->setIdisuario($row['idusuario']);
-       $this->setDeslogin($row['deslogin']);
-       $this->setDessenha($row['dessenha']);
-       $this->setDtcadastro(new DateTime($row['dtcadastro']));
- 	}
- 
-	}
+    public function loadById($id)
+    {
+        $sql = new Sql();
+        $results = $sql->select("SELECT * FROM  tb_usuarios WHERE  idusuario = :ID", array(
+            ":ID"=>$id
+        ));
+        if(count($results) > 0)
+        {
+            $this->setData($results[0]);
+        }
+    }
   
-  public function login($login, $password){
-  	$sql = new Sql();
-  	$sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
-        ":LOGIN"=>$login,
-        ":PASSWORD"=>$password
 
-  		));
-  	 if(count($results) > 0) { 
-  	 	
-        $this->setData($results[0]);
-  	 }
-  	 else { 
-  	 	throw new Exception("Login e/ou senha inválidos.");
-  	 	}
 
-  }
+
 
   public static function getList(){
        
        $sql = new Sql();
-       return $sql->select("SELECT * FROM tb_usuarios order by deslogi Vn");
+       return $sql->select("SELECT * FROM tb_usuarios order by deslogin");
   
 
   }
 
 
+
+public function login($login, $password){
+
+    $sql = new Sql();
+    $results = $sql->select("SELECT * FROM  tb_usuarios WHERE  deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+        ":LOGIN"=>$login,
+        ":PASSWORD"=>$password
+));
+    if(count($results) > 0)
+    {
+
+      $this->setData($results[0]);
+    }
+
+    else {
+        throw new Exception("Login e/ou senha inválidos.");
+    }
+
+}
 
 
   public static function search($login){
@@ -90,29 +93,29 @@ private $dtcadastro;
 
   }
 
-  //NESTE TRECHO É POSSÍVEL ENTENDER QUE O COMANDO DA UM SELECT ALL NA tb_usuarios Aonde a tupla deslogin for igual a CONSTANTE (que eu ainda não entendi mt bem o pq de as vezes ela ter :: as vezes somente :), cria um array que possui uma outra constante SEARCH, ou utiliza o valor dlea e atribui a login, porém não entendi também o porque desses dois sinais de porcentagem se somente o . é necessario para concatenar. 
-
 
 
    public function setData($data){
-
-   	$this->setIdusuario($data['idusuario']);
-   	$this->setDeslogin($data['deslogin']);
-   	$this->setDessenha($data['dessenha']);
-   	$this->setDtcadastro(new DateTime($data['dtcadastro'])); 
+       $this->setIdusuario($data['idusuario']);
+       $this->setDeslogin($data['deslogin']);
+       $this->setDessenha($data['dessenha']);
+       $this->setDtcadastro(new DateTime($data['dtcadastro']));
 
    }
 
    public function insert (){
    	$sql = new Sql();
-   	$results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD", array(
-       ':LOGIN'=>$this->getDeslogin(),
-       ':PASSWORD'=>$this->getDessenha()
-   		));
+    $results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+        ':LOGIN'=>$this->getDeslogin(),
+        ':PASSWORD'=>$this->getDessenha()
+    ));
+
+    if(count ($results) >0 )
+    {
+        $this->setData($results[0]);
+    }
 
    }
-
-
 
   public function __toString()
     {
--- htdocs/DAO/index.php
+++ htdocs/DAO/index.php
@@ -1,33 +1,39 @@
-<?php 
-
+<?php
 require_once("config.php");
 
-//CARREGA UM USUARIO
-/*
+/*CARREGA UM USUARIO, OK
+
 $root = new Usuario(); 
-$root->loadById(1);
-echo ($root); 
+$root->loadById(4);
+echo $root;
+echo "<br>";
+echo json_encode($root);
+/*
+
+ CARREGA UMA LIST DE USUARIOS, OK
+ $lista = Usuario::getList();
+ echo json_encode($lista);
 */
 
+/* CARREGA UMA LISTA DE USUARIOS UTILIZANDO BUSCA NOMINAL PELO LOGIN, OK
+$search = Usuario::search("dic");
+echo json_encode($search);
+*/
 
-// CARREGA UMA LIST DE USUARIOS
-// $lista = Usuario::getList();
-// echo json_encode($lista);
- 
-// CARREGA UMA LISTA DE USUARIOS UTILIZANDO BUSCA NOMINAL PELO LOGIN
-// $search = Usuario::search("dic");
-// echo json_encode($search);
-
+/*CARREGA UM USUARIO USANDO O LOGIN E A SENHA
+$usuario = new Usuario();
+$usuario->login("root", "root");
+echo $usuario;
+*/
 
 
 //DA UM INSERT NO BANCO UTILIZANDO PROCEDURES, CRIA UMA NOVA INSTANCIA DE USUARIO
 $aluno = new Usuario();
 $aluno->setDeslogin("aluno");
 $aluno->setDessenha("@aluno");
-
 $aluno->insert();
-
 echo $aluno;
+
 
 
  ?>
